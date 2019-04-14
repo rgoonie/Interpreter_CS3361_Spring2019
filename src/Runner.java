fdd0e4624d5ed981a4947b64256cd453d9d98941
rgoonie <rajeev.goonie98@gmail.com>
2019-04-14 10:28:12 -0500

    add header and in line comments

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,9 @@
+/**
+ *  @author Rajeev Goonie
+ * 
+ *  This file is the runner for the interpreter. This application will run in command line/bash
+ * 
+ */
 
 import java.util.Scanner;
 
@@ -5,29 +11,38 @@ public class Runner {
 
     public static void main(String[] args) {
         
+        //keyboard input
         Scanner kb = new Scanner(System.in);
         String input;
+        
+        //Syntax Analyzer and Semantic Runner
         LexicalAnalyzer syntax = new LexicalAnalyzer();
         SemanticAnalyzer semantics = new SemanticAnalyzer();
         
-        System.out.println("----------------------------");
-        System.out.println("The Rajeev Project IDLE v3.5");
-        System.out.println("----------------------------");
+        //
+        System.out.println("-------------------------------------------");
+        System.out.println("The Rajeev Project - Boolean Evaluator v3.5");
+        System.out.println("-------------------------------------------");
         
         
         while(true){
             System.out.print(">>>");
             
+            //collects input until a termination symbol (period '.' ) is within input
             input = kb.nextLine();
             while( !input.contains(".") )
                 input += kb.nextLine();
             
             
-            Boolean[] output = {null};
-            
+            //Check syntax before executing code (Errors will be printed from LexicalAnalyzer)
             if(syntax.isCorrect(input)){
-                
+                //Debug statement
                 //System.out.println("\n\nConsiseCode" + consiseCode[0] + "\n\n");
+                
+                //used to return input incase there is a runtime error (undefined variable is used)
+                Boolean[] output = {null};
+                
+                //executes code and prints output (Errors will get print from the SemanticAnalyzer)
                 semantics.executeCode(input, output);
                 if(output[0] != null)
                     System.out.println(output[0] + "\n");
